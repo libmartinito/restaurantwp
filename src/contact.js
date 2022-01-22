4f7893e31a346a850b8ae97c8b8219fb43fe8cfc
Lib <lib.martinito@pm.me>
2022-01-22 12:02:25 +0800

    Added the third page

---
+++ src/contact.js
@@ -0,0 +1,110 @@
+import "./contact-style.css";
+
+const createContactPage = () => {
+  let make = (elem) => {
+    return document.createElement(elem);
+  };
+  
+  let addClass = (elem, str) => {
+    elem.classList.add(str);
+  };
+  
+  let append = (parent, child) => {
+    parent.appendChild(child);
+  };
+
+  let content = make("div");
+  content.id = "contact-content";
+  
+  let header = make("div");
+  addClass(header, "contact-header");
+  
+  let logo = make("div");
+  addClass(logo, "contact-logo");
+  
+  let tabContainer = make("div");
+  addClass(tabContainer, "contact-tab-container");
+  
+  let tabOne = make("div");
+  addClass(tabOne, "tab-one");
+  tabOne.textContent = "Home";
+  
+  let tabTwo = make("div");
+  addClass(tabTwo, "tab-two");
+  tabTwo.textContent = "Menu";
+  
+  let tabThree = make("div");
+  addClass(tabThree, "tab-three");
+  tabThree.textContent = "Contact";
+  
+  append(tabContainer, tabOne);
+  append(tabContainer, tabTwo);
+  append(tabContainer, tabThree);
+  append(header, logo);
+  append(header, tabContainer);
+
+  let contactBody = make("div");
+  addClass(contactBody, "contact-body");
+
+  let title = make("div");
+  addClass(title, "contact-title");
+
+  let bodyOne = make("div");
+  addClass(bodyOne, "contact-body-one");
+  bodyOne.textContent = "How can we help you?";
+
+  let bodyTwo = make("div");
+  addClass(bodyTwo, "contact-body-two");
+  bodyTwo.textContent = "Got a question, comment, or suggestion?";
+
+  let bodyThree = make("div");
+  addClass(bodyThree, "contact-body-three");
+  bodyThree.textContent = "You can email us your concerns at writeus@ph.mcd.com";
+
+  let bodyFour = make("div");
+  addClass(bodyFour, "contact-body-four");
+  bodyFour.textContent = "You can also reach us at our socials below.";
+
+  let socials = make("div");
+  addClass(socials, "socials");
+
+  let fbAnchor = make("a");
+  fbAnchor.href = "https://web.facebook.com/McDo.ph/?_rdc=1&_rdr";
+
+  let fb = make("div");
+  addClass(fb, "fb");
+
+  let instaAnchor = make("a");
+  instaAnchor.href = "https://www.instagram.com/mcdo_ph/?hl=en";
+
+  let insta = make("div");
+  addClass(insta, "insta");
+
+  let twitterAnchor = make("a");
+  twitterAnchor.href = "https://twitter.com/McDo_PH";
+
+  let twitter = make("div");
+  addClass(twitter, "twitter");
+
+  append(fbAnchor, fb);
+  append(instaAnchor, insta);
+  append(twitterAnchor, twitter);
+  append(socials, fbAnchor);
+  append(socials, instaAnchor);
+  append(socials, twitterAnchor);
+
+  append(contactBody, title);
+  append(contactBody, bodyOne);
+  append(contactBody, bodyTwo);
+  append(contactBody, bodyThree);
+  append(contactBody, bodyFour);
+  append(contactBody, socials);
+
+  append(content, header);
+  append(content, contactBody);
+
+  let body = document.querySelector("body");
+  body.prepend(content);
+};
+
+export {createContactPage};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {createHomePage} from "./home.js";
 import {createMenuPage} from "./menu.js";
+import {createContactPage} from "./contact.js";
 
 createHomePage();
 
@@ -14,31 +15,47 @@ tabOne.addEventListener("click", (e) => {
   createHomePage();
   addListener();
 });
+
 tabTwo.addEventListener("click", () => {
   let body = document.querySelector("body");
   let child = body.firstChild;
   body.removeChild(child);
   createMenuPage();
+  addListener();
+});
+
+tabThree.addEventListener("click", () => {
+  let body = document.querySelector("body");
+  let child = body.firstChild;
+  body.removeChild(child);
+  createContactPage();
   addListener();
 });
 
 let addListener = () => {
   let tabOne = document.querySelector(".tab-one");
-let tabTwo = document.querySelector(".tab-two");
-let tabThree = document.querySelector(".tab-three");
+  let tabTwo = document.querySelector(".tab-two");
+  let tabThree = document.querySelector(".tab-three");
 
-tabOne.addEventListener("click", (e) => {
-  let body = document.querySelector("body");
-  let child = body.firstChild;
-  body.removeChild(child);
-  createHomePage();
-  addListener();
-});
-tabTwo.addEventListener("click", () => {
-  let body = document.querySelector("body");
-  let child = body.firstChild;
-  body.removeChild(child);
-  createMenuPage();
-  addListener();
-});   
-}
+  tabOne.addEventListener("click", (e) => {
+    let body = document.querySelector("body");
+    let child = body.firstChild;
+    body.removeChild(child);
+    createHomePage();
+    addListener();
+  });
+  tabTwo.addEventListener("click", () => {
+    let body = document.querySelector("body");
+    let child = body.firstChild;
+    body.removeChild(child);
+    createMenuPage();
+    addListener();
+  });
+  tabThree.addEventListener("click", () => {
+    let body = document.querySelector("body");
+    let child = body.firstChild;
+    body.removeChild(child);
+    createContactPage();
+    addListener();   
+  });
+};
